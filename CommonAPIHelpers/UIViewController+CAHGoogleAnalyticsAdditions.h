64904801f7d4624feb2c313387cf50d764d5b952
Udit Agarwal <uditiiita@gmail.com>
2015-05-26 16:53:49 +0530

    Add documentation

--- CommonAPIHelpers/UIViewController+CAHGoogleAnalyticsAdditions.h
+++ CommonAPIHelpers/UIViewController+CAHGoogleAnalyticsAdditions.h
@@ -4,6 +4,11 @@
  This adds analytics functionality to all of your view controller classes.
  Event for any view controller presented will be sent to google analytics 
  automatically.
+ 
+ @pre If you add this file to your project, be sure that you are setting
+ trackingID on [CAHGoogleAnalytics sharedInstance] otherwise it will crash.
+ 
+ Example: [[CAHGoogleAnalytics sharedInstance] setTrackingID:@"UA-00000000-0"];
  */
 @interface UIViewController (CAHGoogleAnalyticsAdditions)
 
